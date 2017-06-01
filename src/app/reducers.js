1d8730d231a2efedfb73325074e2f579e4d4c75a
Luiz Henrique Soares <luizhenriquesoares91@gmail.com>
2017-05-31 18:24:12 -0800

    added stores and reducers

---
+++ src/app/reducers.js
@@ -0,0 +1,22 @@
+import { combineReducers } from 'redux';
+
+const CREATE_TODO = 'CREATE_TODO';
+
+const user = (state = {}, action) => {
+    return state;
+};
+
+const todos = (state = [], action) => {
+    switch (action.type) {
+        case CREATE_TODO:
+            return [
+                action.payload,
+                ...state
+            ];
+        default:
+        return state;
+    }
+};
+
+
+export default reducer = combineReducers({todos, user});
