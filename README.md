e80f5c6a94cb4a500199a43e97584bd48762c21f
Luiz Henrique Soares <luizhenriquesoares91@gmail.com>
2017-06-05 17:39:14 -0800

    added readme.md

---
+++ README.md
@@ -0,0 +1,8 @@
+## Necessários para instalar
+
+1. node -v 8.0
+2. npm  4.6
+
+## Passos para instalação
+
+> npm install & react-native run-android
--- index.android.js
+++ index.android.js
@@ -7,7 +7,6 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet } from 'react-native';
 import { Todo } from './src/app/Todo';
-import { Fancy } from './src/app/Fancy'
 
 const Main = () => (<Todo />);
 
